6fe538a6c565240afc33a7e9272204519780b7e5
f1uff3h <peco@camarad.tech>
2024-10-27 15:36:07 +0200

    postconfig

--- postconfig.sh
+++ postconfig.sh
@@ -4,13 +4,15 @@ RED='\e[0;31m'
 NC='\e[0m'
 
 if [ "$EUID" -ne 0 ]; then
-	echo -e "\n${RED}[ERROR]Please run as root!${NC}"
-	exit 1
+  echo -e "\n${RED}[ERROR]Please run as root!${NC}"
+  exit 1
 fi
 
+# TODO: move below to archsin
 echo -e "\n[INFO] -- Configuring firewalld..."
 firewall-cmd --zone=public --add-service=kdeconnect --permanent
 
+# TODO: configure celeste and drop these
 echo -e "\n[INFO] -- Adding NFS to fstab..."
 cat <<-EOT >>/etc/fstab
 	# /home/peco/Documents
@@ -22,9 +24,7 @@ cat <<-EOT >>/etc/fstab
 	# /home/peco/Videos
 	nfs.camarad.tech:/mnt/storage/peco/Videos       /home/peco/Videos       nfs     rw,noauto,user,relatime,soft,timeo=100,retrans=2                0       0
 	# /home/peco/repos
-	nfs.camarad.tech:/mnt/storage/peco/repos        /home/peco/repos        nfs     rw,noauto,user,relatime,soft,timeo=100,retrans=2                0       0
-	# /home/peco/ssh
-	nfs.camarad.tech:/mnt/storage/peco/ssh          /home/peco/.ssh         nfs     rw,noauto,user,relatime,soft,timeo=100,retrans=2                0       0
+	nfs.camarad.tech:/mnt/storage/peco/repos        /home/peco/repos        nfs     rw,noauto,user,dev,exec,relatime,soft,timeo=100,retrans=2                0       0
 EOT
 
 echo -e "\n[INFO] -- Adding NFS mount script..."
@@ -35,7 +35,6 @@ cat <<-EOT >/home/peco/bin/nfsmount.sh
 	mount /home/peco/Pictures/
 	mount /home/peco/Videos/
 	mount /home/peco/repos/
-	mount /home/peco/.ssh/
 EOT
 chmod +x /home/peco/bin/nfsmount.sh
 chown peco:peco /home/peco/bin/nfsmount.sh
@@ -54,6 +53,7 @@ cat <<-EOT >/home/peco/.config/autostart/NFSMount.desktop
 EOT
 chown peco:peco /home/peco/.config/autostart/NFSMount.desktop
 
+# TODO: configure flatpak backup and drop this
 echo -e "\n[INFO] -- Adding backup script..."
 cat <<-EOT >/home/peco/bin/backup.sh
 	#!/bin/bash
@@ -121,17 +121,19 @@ EOT
 
 systemctl daemon-reload
 
-if dmidecode -s system-product-name | grep -q "ThinkPad T15 Gen 1" || dmidecode -s system-product-name | grep -q "20S6003NRI"; then
-	echo -e "\n[INFO] -- Thinkpad T15 detected"
+if dmidecode -s system-family | grep -q "ThinkPad T15 Gen 1" || dmidecode -s system-product-name | grep -q "20S6003NRI"; then
+  echo -e "\n[INFO] -- Thinkpad T15 detected"
 
-	pacman -S sof-firmware
+  echo -e "\n[INFO] -- Installing sof-firmware..."
+  pacman -S sof-firmware
 
-	cat <<-EOT >/etc/modprobe.d/disable-nouveau.conf
+  echo -e "\n[INFO] -- Disabling NVIDIA GPU..."
+  cat <<-EOT >/etc/modprobe.d/disable-nouveau.conf
 		blacklist nouveau
 		options nouveau modeset=0
 	EOT
 
-	cat <<-EOT >/etc/udev/rules.d/00-remove-nvidia.rules
+  cat <<-EOT >/etc/udev/rules.d/00-remove-nvidia.rules
 		# Remove NVIDIA USB xHCI Host Controller devices, if present
 		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
 
@@ -145,3 +147,5 @@ if dmidecode -s system-product-name | grep -q "ThinkPad T15 Gen 1" || dmidecode 
 		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
 	EOT
 fi
+
+# TODO: add surface pro 3 setup here
