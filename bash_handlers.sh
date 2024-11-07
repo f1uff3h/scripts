c9f382b7fc9da1bad9492fce0d398b9aa5393016
f1uff3h <peco@camarad.tech>
2024-11-07 08:42:00 +0200

    #9 override log location

--- bash_handlers.sh
+++ bash_handlers.sh
@@ -2,6 +2,7 @@ log() {
   local nc='\e[0m'
   local red='\e[31m'
   local green='\e[0;32m'
+  local yellow='\e[0;33m'
   local log_timestamp
   log_timestamp=$(date +"[%Y-%m-%dT%H:%M:%S]")
 
@@ -9,12 +10,15 @@ log() {
   info)
     echo "${log_timestamp} [INFO] -- ${2}..."
     ;;
+  success)
+    echo -e "${log_timestamp} [${green}SUCCESS${nc}] -- ${2}!"
+    ;;
+  warning)
+    echo -e "${log_timestamp} [${yellow}WARNING${nc}] -- ${2}!"
+    ;;
   error)
     echo -e "${log_timestamp} [${red}ERROR${nc}] -- ${2}!"
     exit 1
-    ;;
-  success)
-    echo -e "${log_timestamp} [${green}SUCCESS${nc}] -- ${2}!"
     ;;
   *)
     echo -e "${log_timestamp} [UNKNOWN] -- INVALID LOG TYPE ${1}..."
@@ -25,7 +29,15 @@ log() {
 readonly -f log
 
 handle_log() {
-  exec &>> >(tee -a "/var/log/$(basename "${0}")")
+  local log_path
+
+  if [ -z "${1}" ]; then
+    log_path="/var/log/$(basename "${0}")"
+  else
+    log_path="${1}"
+  fi
+
+  exec &>> >(tee -a "${log_path}")
 }
 readonly -f handle_log
 
